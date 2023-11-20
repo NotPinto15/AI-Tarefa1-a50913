1b13d21a59b996d4b846991fc4f8cf11cd33110a
Henrique Pinto <126677028+NotPinto15@users.noreply.github.com>
2023-11-20 15:24:48 +0000

    Following a Tracker - Complete

--- Assets/FollowWP.cs
+++ Assets/FollowWP.cs
@@ -1,4 +1,3 @@
-using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,25 +9,48 @@ public class FollowWP : MonoBehaviour
 
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
+    public float lookAhead = 40.0f;
+
+    GameObject tracker; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
+    }
+
+    void ProgressTracker()
+    {
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
+
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
+        {
+            currentWP++;
+        }
+
+        if (currentWP >= waypoints.Length)
+        {
+            currentWP = 0;
+        }
+
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, (speed + 4) * Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (UnityEngine.Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
-            currentWP ++;
-        
-        if(currentWP >= waypoints.Length)
-           currentWP = 0;
+        ProgressTracker();
 
-        // this.transform.LookAt(waypoints[currentWP].transform);
-        UnityEngine.Quaternion lookatWP = UnityEngine.Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
-        
-        this.transform.rotation = UnityEngine.Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+        this.transform.LookAt(waypoints[currentWP].transform);
+
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
 
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
